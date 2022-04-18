1ba0a71191be1ebe5938b5bc8c3b772ddfbcc381
joe <joe.kaikaty@gmail.com>
2022-04-18 03:02:00 +0300

    update download dialog

--- components/DownloadDialog.vue
+++ components/DownloadDialog.vue
@@ -21,15 +21,7 @@
         </v-img>
       </v-col>
       <v-col cols="12" md="8">
-        <!-- <v-card flat class="pa-0">
-          <v-card-title class="pa-1 subtitle-1">{{
-            infos.videoDetails.title
-          }}</v-card-title>
-          <v-card-subtitle class="pa-1 subtitle-2 grey--text">
-            {{ infos.videoDetails.author }}
-          </v-card-subtitle>
-        </v-card> -->
-         <v-card class="d-flex" flat tile>
+        <v-card class="d-flex" flat tile>
           <v-card-title class="pa-0" v-snip="{ lines: 2 }">{{
             infos.videoDetails.title
           }}</v-card-title>
@@ -38,25 +30,46 @@
           <v-list-item-avatar>
             <NuxtLink
               class="nuxt-link-exact-active"
-              :to="{ name: 'channel-id', params: { id: infos.videoDetails.channelId } }"
+              :to="{
+                name: 'channel-id',
+                params: { id: infos.videoDetails.channelId },
+              }"
             >
               <v-avatar size="56">
-                <img :src="infos.videoDetails.thumbnail.thumbnails[infos.videoDetails.thumbnail.thumbnails.length-1].url" />
+                <img
+                  :src="
+                    infos.videoDetails.thumbnail.thumbnails[
+                      infos.videoDetails.thumbnail.thumbnails.length - 1
+                    ].url
+                  "
+                />
               </v-avatar>
             </NuxtLink>
           </v-list-item-avatar>
           <v-list-item-content>
-            <v-list-item-title>{{ infos.videoDetails.author }}</v-list-item-title>
+            <v-list-item-title>{{
+              infos.videoDetails.author
+            }}</v-list-item-title>
             <v-list-item-subtitle v-if="infos.videoDetails.viewCount"
-              >{{ getPlayCounts(infos.videoDetails.viewCount) + " views - "  + convertTime(infos.videoDetails.lengthSeconds)}}
+              >{{
+                getPlayCounts(infos.videoDetails.viewCount) +
+                " views - " +
+                convertTime(infos.videoDetails.lengthSeconds)
+              }}
             </v-list-item-subtitle>
           </v-list-item-content>
           <v-list-item-action>
-            <v-chip v-if="infos.videoDetails.isLiveContent === true" dark small color="red">LIVE</v-chip>
+            <v-chip
+              v-if="infos.videoDetails.isLiveContent === true"
+              dark
+              small
+              color="red"
+              >LIVE</v-chip
+            >
           </v-list-item-action>
         </v-list-item>
       </v-col>
-    
+
       <v-col cols="12">
         <v-card-actions class="pa-0 d-flex justify-end">
           <v-btn
@@ -263,7 +276,7 @@ export default {
     formatNumbers(nb) {
       return utils.bytesToSize(nb);
     },
-        getPlayCounts(nb) {
+    getPlayCounts(nb) {
       return utils.formatNumbers(nb);
     },
     convertTime(time) {
